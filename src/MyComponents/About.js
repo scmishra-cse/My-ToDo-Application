7ff97066aa7ba904ca58388bc5d87833e3379aea
Shubham Chandra Mishra <84576053+scmishra-cse@users.noreply.github.com>
2022-01-13 12:53:44 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>This is an about page.</p> 
+            <p>This Page is to write Todos to remembers things. It it made up by Shubham Chandra Mishra with the help of CodeWithHerry tutorials. </p>
+            <p>Happy Coding</p>
+        </div>
+    )
+}
